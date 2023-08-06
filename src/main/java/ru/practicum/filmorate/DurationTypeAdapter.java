1db11f428d5890182113d0782b1656d4cb44b43f
Evgeniy Rakhelson <zhenkalexandrovich@yandex.ru>
2023-08-06 13:25:26 +0300

    Initial Commit

--- src/main/java/ru/practicum/filmorate/DurationTypeAdapter.java
+++ src/main/java/ru/practicum/filmorate/DurationTypeAdapter.java
@@ -4,12 +4,8 @@ import com.google.gson.*;
 
 import java.lang.reflect.Type;
 import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class DurationTypeAdapter implements JsonSerializer<Duration>, JsonDeserializer<Duration> {
-
-    //private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @Override
     public JsonElement serialize(final Duration duration, final Type typeOfSrc,
@@ -19,7 +15,7 @@ public class DurationTypeAdapter implements JsonSerializer<Duration>, JsonDeseri
 
     @Override
     public Duration deserialize(final JsonElement json, final Type typeOfT,
-                                 final JsonDeserializationContext context) throws JsonParseException {
+                                final JsonDeserializationContext context) throws JsonParseException {
         return Duration.ofMinutes(Long.getLong(json.getAsString()));
     }
 }
